9c63a3c139c88e9736c7852b24f52bf3605b111c
Ivan Eduardo Tolentino Salvatierra <walin21@gmail.com>
2025-06-11 05:13:20 -0500

    Añadir logotipos SVG y actualizar estilos para el sitio web de UrsaLink
    - Se añadieron nuevos archivos SVG: logo_light.svg, next.svg, ursa_v03.svg, vercel.svg, window.svg
    - Se introdujo favicon.ico para la aplicación
    - Se crearon estilos CSS globales en globals.css, incluyendo compatibilidad con modo oscuro y animación de parpadeo de neón
    - Se implementó la estructura de diseño en layout.tsx con metadatos para la aplicación
    - Se desarrolló la página principal en page.tsx con efectos de partículas y diseño adaptable
    - Se configuró el CSS de Tailwind en tailwind.config.js para el estilo
    - Se estableció la configuración de TypeScript en tsconfig.json para una mejor comprobación de tipos y resolución de módulos

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "UrsaLink",
+  description: "UrsaLink es una empresa peruana dedicada al desarrollo de software. Diseñamos soluciones digitales a medida que ayudan a empresas y emprendedores a transformar sus ideas en productos reales y eficientes.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="es">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
